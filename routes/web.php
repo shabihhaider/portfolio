e75afbdce9fb224ac901b6582f22480119066aa7
shabihhaider <2022cs723@student.uet.edu.pk>
2025-03-28 11:57:03 -0700

    Update 03

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,6 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
-
 //Route::post('/email', [EmailController::class, 'sendEmail']);
 
 require __DIR__.'/settings.php';
