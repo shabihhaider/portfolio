de79ea0ea209e9b67f43c265e1c4807346e5319e
shabihhaider <2022cs723@student.uet.edu.pk>
2025-03-28 09:56:54 -0700

    Update 1

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,7 +5,10 @@
  */
 
 import axios from 'axios';
+import $ from 'jquery';
 window.axios = axios;
+window.$ = $;
+window.jQuery = $;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
